818faacbf8ffa07029e760d2b6a1745da37b5c3a
denchai_saisupatpon <denchaisai@hotmail.com>
2016-08-31 13:20:03 +0700

    day 3 load image from server finish

--- app/src/main/java/com/saisupatpon/denchai/rdrun/MainActivity.java
+++ app/src/main/java/com/saisupatpon/denchai/rdrun/MainActivity.java
@@ -4,13 +4,35 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class  MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private ImageView imageView;
+    private EditText userEditText,passwoEditText;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //bind widget
+        imageView = (ImageView) findViewById(R.id.imageView6);
+        userEditText = (EditText) findViewById(R.id.editText5);
+        passwoEditText = (EditText) findViewById(R.id.editText6);
+
+        //load image from server
+        Picasso.with(this).load("http://www.swiftcodingthai.com/rd/Image/rd_logo.png")
+        .resize(150,150).into(imageView);
+
     } //Main Method นี่คือเมทธอด
 
     public void clickSignInMain(View view) {
